65bf940038799e55074dffdf09bb4404c26bee7f
gvnnz <monocasual@gmail.com>
2025-11-25 18:57:54 +0100

    Add tests

---
+++ tests/all.cpp
@@ -0,0 +1,56 @@
+#include "src/fs.hpp"
+#include "src/math.hpp"
+#include "src/string.hpp"
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+
+TEST_CASE("fs")
+{
+	using namespace mcl::utils::fs;
+
+	REQUIRE(fileExists("nonexistent_file") == false);
+	REQUIRE(dirExists("ghost_dir/") == false);
+	REQUIRE(isDir("nonexistent_dir") == false);
+	// REQUIRE(basename("tests/utils.cpp") == "utils.cpp"); // ????
+	REQUIRE(dirname("tests/utils.cpp") == "tests");
+	REQUIRE(getExt("tests/utils.cpp") == ".cpp");
+	REQUIRE(stripExt("tests/utils.cpp") == "tests/utils");
+#if defined(_WIN32)
+	REQUIRE(isRootDir("C:\\path\\to\\something") == false);
+	REQUIRE(getUpDir("C:\\path\\to\\something") == "C:\\path\\to");
+	REQUIRE(getUpDir("C:\\path") == "C:\\");
+#else
+	REQUIRE(isRootDir("/") == true);
+	REQUIRE(isRootDir("/path/to/something") == false);
+	REQUIRE(getUpDir("/path/to/something") == "/path/to");
+	REQUIRE(getUpDir("/path") == "/");
+	REQUIRE(getUpDir("/") == "/");
+#endif
+}
+
+TEST_CASE("string")
+{
+	using namespace mcl::utils::string;
+
+	REQUIRE(replace("This is cool", "cool", "hot") == "This is hot");
+	REQUIRE(trim("   This is cool       ") == "This is cool");
+
+	std::vector<std::string> v = split("This is cool", " ");
+	REQUIRE(v.size() == 3);
+	REQUIRE(v.at(0) == "This");
+	REQUIRE(v.at(1) == "is");
+	REQUIRE(v.at(2) == "cool");
+}
+
+TEST_CASE("math")
+{
+	using namespace mcl::utils::math;
+
+	REQUIRE(map(0.0f, 0.0f, 30.0f, 0.0f, 1.0f) == 0.0f);
+	REQUIRE(map(30.0f, 0.0f, 30.0f, 0.0f, 1.0f) == 1.0f);
+	REQUIRE_THAT(map(15.0f, 0.0f, 30.0f, 0.0f, 1.0f), Catch::Matchers::WithinAbs(0.5f, 0.001f));
+
+	REQUIRE(map(0.0f, 30.0f, 1.0f) == 0.0f);
+	REQUIRE(map(30.0f, 30.0f, 1.0f) == 1.0f);
+	REQUIRE_THAT(map(15.0f, 30.0f, 1.0f), Catch::Matchers::WithinAbs(0.5f, 0.001f));
+}
